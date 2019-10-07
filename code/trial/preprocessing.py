9059e8e11184b556adb20c9cbbefec06f10f5fc7
sendai0905 <coom_st@ruri.waseda.jp>
2019-10-08 04:01:38 +0900

    mutagenのインストール・活用

--- code/trial/preprocessing.py
+++ code/trial/preprocessing.py
@@ -2,21 +2,23 @@ from pathlib import Path, PurePath
 import numpy as np
 import re
 from collections import Counter
+from mutagen.easyid3 import EasyID3
 
 
 def makeComposerList():
     p = Path(r"C:\Users\Takumi\Desktop\statistic\data")
-    wav_list = sorted(list(p.glob("*.wav")))
-    filename_list = [PurePath(str(x)).stem for x in wav_list]
+    mp3_list = sorted(list(p.glob("*.mp3")))
+    # filename_list = [PurePath(str(x)).stem for x in wav_list]
 
     # print(filename_list)
 
     composer_list = []
-    for x in filename_list:
-        if re.match(r"[0-9]", x.split("-")[0]):
-            composer_list.append(x.split("-")[1])
-        else:
-            composer_list.append(x.split("-")[0])
+    for x in mp3_list:
+        # if re.match(r"[0-9]", x.split("-")[0]):
+        #     composer_list.append(x.split("-")[1])
+        # else:
+        #     composer_list.append(x.split("-")[0])
+        composer_list.append(EasyID3(x)['composer'])  # 'artist'かも
 
     composer_list.sort()
     num_of_composer = Counter(composer_list)
