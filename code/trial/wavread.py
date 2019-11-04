2342f8e6c26867988c08ab0b25e51430e09d1db9
sendai0905 <coom_st@ruri.waseda.jp>
2019-11-05 03:18:57 +0900

    前処理詰め

--- code/trial/wavread.py
+++ code/trial/wavread.py
@@ -1,12 +1,14 @@
-from preprocessing import makeComposerList
-from keras.models import Sequential
-from keras.layers import Input, Dense, concatenate
-from keras.layers.recurrent import LSTM
-from keras.callbacks import EarlyStopping
-import numpy as np
+import glob
+
 import librosa
 import librosa.display
 import matplotlib.pyplot as plt
+import numpy as np
+from keras.callbacks import EarlyStopping
+from keras.layers import Dense, Input, concatenate
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
+from mutagen.easyid3 import EasyID3
 
 
 def readwave(file):
@@ -18,22 +20,43 @@ def MelFilterBank(y, sr):
     return librosa.feature.melspectrogram(y=y, sr=sr)
 
 
-def makeLSTMmodel(hidden):
+def makeArtistList():
+    path = "C:/Users/Takumi/Desktop/statistic/data/mp3/"
+    mp3_list = sorted(list(glob.glob(path + "*.mp3")))
+    artist_list = []
+    print(EasyID3(mp3_list[0]))
+    # for file in mp3_list:
+    #     try:
+    #         artist_list.append(EasyID3(file)['artist'])
+    #         if not len(artist_list) % 100:
+    #             print(len(artist_list))
+    #     except Exception as e:
+    #         print(file, e.args)
+    #         continue
+    # return artist_list
+
+
+def makeLSTMmodel(hidden=128, input_shape=(128,)):
     model = Sequential()
-    model.add(LSTM(hidden, input_shape=(),))
-    model.add(Dense())
-    model.compile(loss="categorical_crossentropy", optimezer="")
+    model.add(LSTM(hidden, input_shape=input_shape))
+    model.add(Dense(128))
+    model.compile(loss="categorical_crossentropy", optimezer="adam")
     return model
 
 
 if __name__ == "__main__":
-    wav, sr = readwave("J.S.Bach-invention-No.1-Harpsi.wav")
-    print(wav.shape)
-    print(sr)
-    t = np.arange(0, len(wav)) / sr
-    m = MelFilterBank(wav, sr)
-    print(m)
-    print(m.shape)
+    makeArtistList()
+    # print(makeArtistList())
+    # path = "C:/Users/Takumi/Desktop/statistic/data/wav/"
+    # wav_list = sorted(list(glob.glob(path + "*.wav")))
+    # for i in range(3):
+    #     wav, sr = readwave(wav_list[i])
+    #     print(wav.shape)
+    #     print(sr)
+    #     t = np.arange(0, len(wav)) / sr
+    #     m = MelFilterBank(wav, sr)
+    #     print(m)
+    #     print(m.shape)
 
     # 波形をプロット
     # plt.plot(m)
@@ -41,10 +64,10 @@ if __name__ == "__main__":
     # plt.ylabel("amplitude")
     # plt.savefig("waveform.png")
     # plt.show()
-    librosa.display.specshow(librosa.power_to_db(
-        m, ref=np.max), y_axis='mel', fmax=8000, x_axis='time')
-    plt.colorbar(format='%+2.0f dB')
-    plt.title('Mel spectrogram')
-    plt.tight_layout()
-    plt.savefig("waveform.png")
-    plt.show()
+    # librosa.display.specshow(librosa.power_to_db(
+    #     m, ref=np.max), y_axis='mel', fmax=8000, x_axis='time')
+    # plt.colorbar(format='%+2.0f dB')
+    # plt.title('Mel spectrogram')
+    # plt.tight_layout()
+    # plt.savefig("waveform.png")
+    # plt.show()
